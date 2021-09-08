08715bf2c806967f3a9d8aee1b4766d49e8d7d60
GenerelSchwerz <xXSG.KingXx@gmail.com>
2021-09-08 12:51:36 -0400

    12th test

--- dist/index.js
+++ dist/index.js
@@ -13220,7 +13220,7 @@ const newRequest = async ({url, method, data, http2, body, headers}) => {
       throwHttpErrors: false,
       responseType: 'text',
     })
-    actions.debug(JSON.stringify({url, method, data, http2, body, headers}))
+    actions.error(JSON.stringify({url, method, data, http2, body, headers}))
     actions.setOutput('body', JSON.stringify(result.body))
     actions.setOutput('headers', JSON.stringify(result.headers))
 
