84926e8421cbec62f08377b0836ea4453b0eec95
GenerelSchwerz <xXSG.KingXx@gmail.com>
2021-09-08 08:36:24 -0400

    fifth test

--- src/httpClient.js
+++ src/httpClient.js
@@ -69,145 +69,145 @@ const request = async ( url, method = METHOD_POST, data = undefined, { http2 = f
 };
 
 
-const request_old = async({ method, instanceConfig, data, files, auth, actions, ignoredCodes, preventFailureOnNoResponse, escapeData }) => {
-  try {
-    if (escapeData) {
-      data = data.replace(/"[^"]*"/g, (match) => { 
-        return match.replace(/[\n\r]\s*/g, "\\n");
-      }); 
-    }
+// const request_old = async({ method, instanceConfig, data, files, auth, actions, ignoredCodes, preventFailureOnNoResponse, escapeData }) => {
+//   try {
+//     if (escapeData) {
+//       data = data.replace(/"[^"]*"/g, (match) => { 
+//         return match.replace(/[\n\r]\s*/g, "\\n");
+//       }); 
+//     }
 
-    if (method === METHOD_GET) {
-      data = undefined;
-    }
+//     if (method === METHOD_GET) {
+//       data = undefined;
+//     }
 
-    if (files && files !== '{}') {
-      filesJson = convertToJSON(files)
-      dataJson = convertToJSON(data)
+//     if (files && files !== '{}') {
+//       filesJson = convertToJSON(files)
+//       dataJson = convertToJSON(data)
 
-      if (Object.keys(filesJson).length > 0) {
-        try {
-          data = convertToFormData(dataJson, filesJson)
-          instanceConfig = await updateConfig(instanceConfig, data, actions)
-        } catch(error) {
-          actions.setFailed({ message: `Unable to convert Data and Files into FormData: ${error.message}`, data: dataJson, files: filesJson })
-          return
-        }
-      }
-    }
+//       if (Object.keys(filesJson).length > 0) {
+//         try {
+//           data = convertToFormData(dataJson, filesJson)
+//           instanceConfig = await updateConfig(instanceConfig, data, actions)
+//         } catch(error) {
+//           actions.setFailed({ message: `Unable to convert Data and Files into FormData: ${error.message}`, data: dataJson, files: filesJson })
+//           return
+//         }
+//       }
+//     }
 
-    const requestData = {
-      auth,
-      method,
-      data,
-      maxContentLength: Infinity,
-      maxBodyLength: Infinity
-    }
+//     const requestData = {
+//       auth,
+//       method,
+//       data,
+//       maxContentLength: Infinity,
+//       maxBodyLength: Infinity
+//     }
 
-    actions.debug('Instance Configuration: ' + JSON.stringify(instanceConfig))
+//     actions.debug('Instance Configuration: ' + JSON.stringify(instanceConfig))
     
-    const instance = axios.create(instanceConfig);
+//     const instance = axios.create(instanceConfig);
 
-    actions.debug('Request Data: ' + JSON.stringify(requestData))
+//     actions.debug('Request Data: ' + JSON.stringify(requestData))
 
-    const response = await instance.request(requestData)
+//     const response = await instance.request(requestData)
 
-    actions.setOutput('response', JSON.stringify(response.data))
-  } catch (error) {
-    if (error.toJSON) {
-      actions.setOutput('requestError', JSON.stringify(error.toJSON()));
-    }
+//     actions.setOutput('response', JSON.stringify(response.data))
+//   } catch (error) {
+//     if (error.toJSON) {
+//       actions.setOutput('requestError', JSON.stringify(error.toJSON()));
+//     }
 
-    if (error.response && ignoredCodes.includes(error.response.status)) {
-      actions.warning(JSON.stringify({ code: error.response.status, message: error.response.data }))
-    } else if (error.response) {
-      actions.setFailed(JSON.stringify({ code: error.response.status, message: error.response.data }))
-    } else if (error.request && !preventFailureOnNoResponse) {
-      actions.setFailed(JSON.stringify({ error: "no response received" }));
-    } else if (error.request && preventFailureOnNoResponse) {
-      actions.warning(JSON.stringify(error));
-    } else {
-      actions.setFailed(JSON.stringify({ message: error.message, data }));
-    }
-  }
-}
+//     if (error.response && ignoredCodes.includes(error.response.status)) {
+//       actions.warning(JSON.stringify({ code: error.response.status, message: error.response.data }))
+//     } else if (error.response) {
+//       actions.setFailed(JSON.stringify({ code: error.response.status, message: error.response.data }))
+//     } else if (error.request && !preventFailureOnNoResponse) {
+//       actions.setFailed(JSON.stringify({ error: "no response received" }));
+//     } else if (error.request && preventFailureOnNoResponse) {
+//       actions.warning(JSON.stringify(error));
+//     } else {
+//       actions.setFailed(JSON.stringify({ message: error.message, data }));
+//     }
+//   }
+// }
 
-/**
- * @param {string} value
- *
- * @returns {Object}
- */
-const convertToJSON = (value) => {
-  try {
-    return JSON.parse(value)
-  } catch(e) {
-    return {}
-  }
-}
+// /**
+//  * @param {string} value
+//  *
+//  * @returns {Object}
+//  */
+// const convertToJSON = (value) => {
+//   try {
+//     return JSON.parse(value)
+//   } catch(e) {
+//     return {}
+//   }
+// }
 
-/**
- * @param {Object} data
- * @param {Object} files
- *
- * @returns {FormData}
- */
-const convertToFormData = (data, files) => {
-  formData = new FormData()
+// /**
+//  * @param {Object} data
+//  * @param {Object} files
+//  *
+//  * @returns {FormData}
+//  */
+// const convertToFormData = (data, files) => {
+//   formData = new FormData()
 
-  for (const [key, value] of Object.entries(data)) {
-    formData.append(key, value)
-  }
+//   for (const [key, value] of Object.entries(data)) {
+//     formData.append(key, value)
+//   }
 
-  for (const [key, value] of Object.entries(files)) {
-    formData.append(key, fs.createReadStream(value))
-  }
+//   for (const [key, value] of Object.entries(files)) {
+//     formData.append(key, fs.createReadStream(value))
+//   }
 
-  return formData
-}
+//   return formData
+// }
 
-/**
- * @param {{ baseURL: string; timeout: number; headers: { [name: string]: string } }} instanceConfig
- * @param {FormData} formData
- * @param {*} actions
- *
- * @returns {{ baseURL: string; timeout: number; headers: { [name: string]: string } }}
- */
-const updateConfig = async (instanceConfig, formData, actions) => {
-  try {
-    const formHeaders = formData.getHeaders()
-    const contentType = formHeaders['content-type']
+// /**
+//  * @param {{ baseURL: string; timeout: number; headers: { [name: string]: string } }} instanceConfig
+//  * @param {FormData} formData
+//  * @param {*} actions
+//  *
+//  * @returns {{ baseURL: string; timeout: number; headers: { [name: string]: string } }}
+//  */
+// const updateConfig = async (instanceConfig, formData, actions) => {
+//   try {
+//     const formHeaders = formData.getHeaders()
+//     const contentType = formHeaders['content-type']
 
-    delete formHeaders['content-type']
+//     delete formHeaders['content-type']
 
-    return { 
-      ...instanceConfig, 
-      headers: { 
-        ...instanceConfig.headers, 
-        ...formHeaders,
-        'Content-Length': await contentLength(formData),
-        'Content-Type': contentType
-      }
-    }
-  } catch(error) {
-    actions.setFailed({ message: `Unable to read Content-Length: ${error.message}`, data, files })
-  }
-}
+//     return { 
+//       ...instanceConfig, 
+//       headers: { 
+//         ...instanceConfig.headers, 
+//         ...formHeaders,
+//         'Content-Length': await contentLength(formData),
+//         'Content-Type': contentType
+//       }
+//     }
+//   } catch(error) {
+//     actions.setFailed({ message: `Unable to read Content-Length: ${error.message}`, data, files })
+//   }
+// }
 
-/**
- * @param {FormData} formData
- *
- * @returns {Promise<number>}
- */
-const contentLength = (formData) => new Promise((resolve, reject) => {
-  formData.getLength((err, length) => {
-    if (err) {
-      reject (err)
-      return
-    }
+// /**
+//  * @param {FormData} formData
+//  *
+//  * @returns {Promise<number>}
+//  */
+// const contentLength = (formData) => new Promise((resolve, reject) => {
+//   formData.getLength((err, length) => {
+//     if (err) {
+//       reject (err)
+//       return
+//     }
 
-    resolve(length)
-  })
-})
+//     resolve(length)
+//   })
+// })
 
 module.exports = {
   request,
