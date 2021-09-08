5a85d664f9c05b542d134ac7c2b820f776411c38
GenerelSchwerz <xXSG.KingXx@gmail.com>
2021-09-08 10:36:39 -0400

    seventh test

--- src/httpClient.js
+++ src/httpClient.js
@@ -58,14 +58,15 @@ const request = async ( url, method, data, { http2, body, headers } = {}) => {
       responseType: 'text',
     })
 
-    actions.setOutput('responseHeaders', JSON.stringify(result.body))
-    actions.setOutput('responseBody', JSON.stringify(result.body))
+    actions.setOutput('headers', JSON.stringify(result.headers))
+    actions.setOutput('response', JSON.stringify(result.body))
 
     return JSON.stringify(result.body);
   } catch (error) {
     if (error.toJSON) {
       actions.setOutput('requestError', JSON.stringify(error.toJSON()));
     } else {
+      console.log(error)
       core.debug(error)
     }
   }
