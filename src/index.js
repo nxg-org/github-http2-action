588dc8c924bf9862d247dd94d4bc64e6c8ec3e66
GenerelSchwerz <xXSG.KingXx@gmail.com>
2021-09-09 14:47:40 -0400

    14th test

--- src/index.js
+++ src/index.js
@@ -30,8 +30,8 @@ exec(`"${join(__dirname, 'github-http-reqs')}" -u=https://httpbin.org/anything -
       core.error(`exec error: ${error}`);
       return;
     }
-    core.setOutput("body", JSON.stringify(`${stdout}`));
-    core.setOutput("headers", JSON.stringify(`${stderr}`));
+    core.setOutput("body", JSON.stringify(`${stdout.replace("\n  ", "")}`));
+    core.setOutput("headers", JSON.stringify(`${stderr.replace("\n  ", "")}`));
   });
 
 
